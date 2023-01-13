d31dd079cba9837968ba9e6cb4913709c959cae1
Roberto Serritella <roberto.serritella@gmail.com>
2023-01-13 11:17:51 +0100

    message...

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Ciao Roberto");
+		hello();
+	}
+	
+	private static void hello() {
+		System.out.println("hello");
 	}
 
 }
