80b71c2478f46be2ca1789ac58d4a76d274cd337
Roberto Serritella <roberto.serritella@gmail.com>
2023-01-13 11:11:19 +0100

    ho aggiunto un messaggio in main.java e creato la classe Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Prova Git");
 		
 		String ciao= "Bye bye";
 		
 		System.out.println(ciao);
+		
+		System.out.println("Messaggio Roberto");
 	}
 
 }
