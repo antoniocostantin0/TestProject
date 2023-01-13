23f67fdc2374644e821a8f265f569e2a98c7e26b
user <user@192.168.1.60>
2023-01-13 10:01:33 +0100

    Ho aggiunto le proprietà e Get and Set

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,4 +1,20 @@
 
 public class Persona {
 
+	private String nome;
+	private String cognome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	
 }
